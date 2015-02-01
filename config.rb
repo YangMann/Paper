bf6f50aaffab722fc0b6b563cc05731eba8e9820
YangMann <jeffreyzhang@vip.qq.com>
2015-02-02 00:45:06 +0800

    created _base & _settings, email secured

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@
 # If you followed directions and ran 'gem install modular-scale' comment the next two lines out:
 extensions_dir = 'sass/extensions'
 
-Compass::Frameworks.register('modular-scale', :path => File.expand_path("#{extensions_dir}/modular-scale"))
+# Compass::Frameworks.register('modular-scale', :path => File.expand_path("#{extensions_dir}/modular-scale"))
 
 # Uncomment these to use regular Ruby gems.
-# require 'modular-scale'
-# require 'sassy-math'
+require 'modular-scale'
+require 'sassy-math'
 
 # Set this to the root of your project when deployed:
 http_path = '/'

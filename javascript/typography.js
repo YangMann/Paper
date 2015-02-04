b28ab5b076dccac91d3a3c4e42585da1439ac212
YangMann <jeffreyzhang@vip.qq.com>
2015-02-04 16:24:13 +0800

    updated Chinese typography components

---
+++ javascript/typography.js
@@ -0,0 +1,16 @@
+function emphasize () {
+    'use strict';
+    $('em').each(function () {
+        var tmp = $(this).text();
+        tmp = tmp.split("");
+        var out = '';
+        for (var i = 0; i < tmp.length; i++) {
+            out += '<em>' + tmp[i] + '</em>';
+        }
+        $(this).replaceWith(out);
+    });
+}
+
+$(function() {
+    emphasize();
+});
